35496116cde21d6318978af9de35874601fc556a
silverpill <silverpill@firemail.cc>
2022-05-18 18:29:33 +0000

    Enable reactive variables

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
+  chainWebpack: (config) => {
+    // Enable reactivity transform
+    // https://github.com/vuejs/rfcs/discussions/369
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        return {
+          ...options,
+          reactivityTransform: true,
+        }
+      })
+  },
   lintOnSave: false,
 }
