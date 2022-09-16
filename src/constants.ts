36d3c5849cb98c2746160b22d7d4ae1498172b2c
silverpill <silverpill@firemail.cc>
2022-09-16 14:57:07 +0000

    Add application-name meta header

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const BACKEND_URL = process.env.VUE_APP_BACKEND_URL
-
 export const ENV = process.env.NODE_ENV
+
+export const BACKEND_URL = process.env.VUE_APP_BACKEND_URL
+export const APP_VERSION = process.env.VUE_APP_VERSION
