6ebf83c76c80d48f6d29c61511809e303e7fb569
silverpill <silverpill@firemail.cc>
2022-12-25 23:06:27 +0000

    Replace markdown on Ethereum page with HTML

--- src/views/Ethereum.vue
+++ src/views/Ethereum.vue
@@ -2,14 +2,19 @@
   <static-page class="wide">
     <template #heading>Ethereum</template>
     <template #text>
-      <div v-html="text"></div>
+      <p>
+        You can register on this server with your <a href="https://ethereum.org/en/wallets/find-wallet/?filters=has_explore_dapps" target="_blank" rel="noopener">Ethereum wallet</a>.
+        <a href="https://metamask.io/" target="_blank" rel="noopener">MetaMask</a> is a good choice for beginners.
+        Your wallet address will be stored for authentication purposes and will not be exposed unless you later complete the address verification procedure.
+        In that case, your address will be publicly associated with your account.
+        To protect your privacy, it is recommended that you create a fresh address and don't link it to your other activities.
+        This can be achieved by using an exchange <a href="http://kycnot.me/" target="_blank" rel="noopener">that doesn't require identity verification</a>.
+        For general privacy recommendations, visit <a href="https://www.privacyguides.org/tools/" target="_blank" rel="noopener">PrivacyGuides</a> website.
+      </p>
     </template>
   </static-page>
 </template>
 
 <script setup lang="ts">
 import StaticPage from "@/components/StaticPage.vue"
-import { renderMarkdown } from "@/utils/markdown"
-
-const text = renderMarkdown("In order to register on this server, you need an [Ethereum wallet](https://ethereum.org/en/wallets/find-wallet/?filters=has_explore_dapps). [MetaMask](https://metamask.io/) is a good choice for beginners. Your wallet address will be stored for authentication purposes and will not be exposed unless you later complete an address verification procedure. In that case, your address will be publicly associated with your account. To protect your privacy, it is recommended that you create a fresh address and don't link it to your other activities. This can be achieved by using a [mixer](https://tornado.cash/) or an exchange [that doesn't require identity verification](http://kycnot.me/). For general privacy recommendations, visit [PrivacyGuides](https://www.privacyguides.org/tools/) website.")
 </script>
