c3af987ff6696722a49bd3d33d0a57956ebba985
silverpill <silverpill@firemail.cc>
2023-08-23 01:39:57 +0000

    Fix lint error

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process')
+const { exec } = require("child_process")
 
 exec(`tar -czvf mitra-web_${process.env.npm_package_version}.tar.gz dist`, (error) => {
   if (error) {
