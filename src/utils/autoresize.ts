3e701656dcbf2475b93c71d5d50022d860660b01
silverpill <silverpill@firemail.cc>
2025-05-31 16:40:28 +0000

    Fix a "shifting textarea" bug in FireFox

--- src/utils/autoresize.ts
+++ src/utils/autoresize.ts
@@ -5,6 +5,12 @@ export function resizeTextArea(textarea: HTMLTextAreaElement) {
   textarea.style.height = `${textarea.scrollHeight}px`
   // Restore scroll position (helps when textarea is at the bottom of the viewport)
   window.scrollTo(x, y)
+  if (window.scrollY === y - 1) {
+    // Workaround for a FireFox bug.
+    // It might change Y by 1px if scrolling to the same position.
+    // Sometimes obverved when there is an image above the textarea.
+    window.scrollTo(x, y + 1)
+  }
 }
 
 // TODO: use https://developer.mozilla.org/en-US/docs/Web/CSS/field-sizing
