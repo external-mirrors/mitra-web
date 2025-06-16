5e8feb1b8b2866232177ec43f666eeea7bd760d0
silverpill <silverpill@firemail.cc>
2025-06-16 09:50:47 +0000

    Create greentext if there is a space after ">" sign

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 ### Fixed
 
 - Fixed broken custom emojis in bio when it is wrapped in `<div>` tag.
+- Create greentext if there is a space after ">" sign.
 
 ## [4.4.0] - 2025-06-05
 
--- tests/unit/greentext.spec.ts
+++ tests/unit/greentext.spec.ts
@@ -11,6 +11,16 @@ describe("Greentext", () => {
     const result = element.innerHTML
     expect(result).to.equal('test<br><span class="greentext">&gt;greentext1</span><br>abc<br><span class="greentext">&gt;greentext2</span><br>xyz')
   })
+
+  it("Should add greentext class if there is a single space after >", () => {
+    const text = "test<br>&gt; a"
+    const element = document.createElement("p")
+    element.innerHTML = text
+    replaceTextNodes(element, addGreentext)
+    const result = element.innerHTML
+    expect(result).to.equal('test<br><span class="greentext">&gt; a</span>')
+  })
+
   it("Should not add greentext class if there is no text", () => {
     const text = "<span>a</span> &gt; <span>b</span>"
     const element = document.createElement("p")
