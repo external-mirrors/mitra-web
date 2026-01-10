cb28af25d248292ea364db9cb9e1bf6a9c7ab3c7
silverpill <silverpill@firemail.cc>
2026-01-10 13:29:25 +0000

    Display unread notification count in page title

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 - Added `--avatar-border-radius` CSS variable for customization.
 - Display message when notification list is empty.
+- Display unread notification count in page title.
 
 ### Changed
 
--- src/composables/title.ts
+++ src/composables/title.ts
@@ -1,16 +1,34 @@
+import { computed, ref, watch } from "vue"
+
 import { useInstanceInfo } from "@/composables/instance"
+import { useNotifications } from "@/composables/notifications"
+
+const TITLE_FALLBACK = "Mitra"
+
+const { getUnreadNotificationCount } = useNotifications()
+
+const title = ref<string>(TITLE_FALLBACK)
+
+const fullTitle = computed(() => {
+  const count = getUnreadNotificationCount() // reactive
+  const prefix = count > 0 ? `(${count})` : ""
+  return `${prefix} ${title.value}`
+})
+
+watch(fullTitle, (value) => {
+  // Safe: https://stackoverflow.com/a/61435099
+  document.title = value
+}, { immediate: true })
 
 export function useTitle() {
-  function setPageTitle(title?: string) {
+  function setPageTitle(pageTitle?: string) {
     const { instance } = useInstanceInfo()
-    const instanceTitle = instance.value?.title || "Mitra"
-    if (title) {
-      title = `${title} | ${instanceTitle}`
+    const instanceTitle = instance.value?.title || TITLE_FALLBACK
+    if (pageTitle) {
+      title.value = `${pageTitle} | ${instanceTitle}`
     } else {
-      title = instanceTitle
+      title.value = instanceTitle
     }
-    // Safe: https://stackoverflow.com/a/61435099
-    document.title = title
   }
 
   return {
